0be9c8a67066b629153a9b2583a207541ce4c2b4
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2020-01-25 10:56:28 -0500

    Included the WiP companion Android app.

---
+++ Phone App/android_app/BiofeedbackStress/app/src/main/java/com/example/biofeedbackstress/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.biofeedbackstress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Probably include the HR rate algorithm in here?
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
